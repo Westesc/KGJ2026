35fd7dcfcf75f4ad527a45a6decbb9581f1b4a95
MAIPA01 <marcelipatryk@gmail.com>
2026-05-09 23:53:20 +0200

    Multidoor

--- Assets/Scripts/Room/RoomInfo.cs
+++ Assets/Scripts/Room/RoomInfo.cs
@@ -1,15 +1,15 @@
 using SaintsField;
 using UnityEngine;
 
-public enum DoorType { Top, Bottom, Right, Left }
+public enum DoorType { Left = 0, Top = 1, Right = 2, Bottom = 3 }
 
 public class RoomInfo : MonoBehaviour
 {
     public int index;
-    public RoomDoors topDoors;
-    public RoomDoors bottomDoors;
-    public RoomDoors leftDoors;
-    public RoomDoors rightDoors;
+    public RoomDoor topDoors;
+    public RoomDoor bottomDoors;
+    public RoomDoor leftDoors;
+    public RoomDoor rightDoors;
 
     public void Rotate(uint cycles)
     {
@@ -23,9 +23,14 @@ public class RoomInfo : MonoBehaviour
     public void RotateOne()
     {
         (topDoors, rightDoors, bottomDoors, leftDoors) = (leftDoors, topDoors, rightDoors, bottomDoors);
+
+        topDoors.SetDoorType(DoorType.Top);
+        rightDoors.SetDoorType(DoorType.Right);
+        bottomDoors.SetDoorType(DoorType.Bottom);
+        leftDoors.SetDoorType(DoorType.Left);
     }
 
-    public void Exit(RoomDoors door)
+    public void Exit(RoomDoor door)
     {
         if (door == null)
         {
