fee069c3828aec041d167242deebc627c73b911f
MAIPA01 <marcelipatryk@gmail.com>
2026-05-09 03:09:31 +0200

    Animacja chodzenia

--- Assets/Scripts/PlayerBody.cs
+++ Assets/Scripts/PlayerBody.cs
@@ -1,7 +1,21 @@
 using UnityEngine;
+using System.Collections.Generic;
+using DG.Tweening;
 
 public class PlayerBody : MonoBehaviour
 {
+    enum State
+    {
+        Idle, MoveUp, MoveDown, MoveLeft, MoveRight
+    }
+
+    [System.Serializable]
+    public struct Animation
+    {
+        public List<Sprite> sprites;
+        public float timeForSprite;
+    }
+
     [SerializeField]
     private PlayerInfo playerInfo;
     private PlayerMovement playerMovement;
@@ -9,46 +23,112 @@ public class PlayerBody : MonoBehaviour
     public SpriteRenderer spriteRenderer;
 
     public Sprite idlePose;
-    public Sprite moveUpPose;
-    public Sprite moveDownPose;
-    public Sprite moveLeftPose;
+    public Animation moveUpAnim;
+    public Animation moveDownAnim;
+    public Animation moveLeftAnim;
+
+    private Sequence currentAnimSequence = null;
+
+    private State currentState = State.Idle;
+    private State CurrentState
+    {
+        get => currentState;
+        set
+        {
+            if (value == currentState)
+            {
+                return;
+            }
+
+            if (value == State.Idle)
+            {
+                StopAnimation();
+                spriteRenderer.sprite = idlePose;
+                currentState = value;
+                return;
+            }
+
+            if ((value == State.MoveLeft && currentState == State.MoveRight) || (value == State.MoveRight && currentState == State.MoveLeft))
+            {
+                spriteRenderer.flipX = !spriteRenderer.flipX;
+                currentState = value;
+                return;
+            }
+
+            spriteRenderer.flipX = false;
+            switch (value)
+            {
+                case State.MoveLeft:
+                    PlayAnimation(moveLeftAnim);
+                    break;
+                case State.MoveRight:
+                    spriteRenderer.flipX = true;
+                    PlayAnimation(moveLeftAnim);
+                    break;
+                case State.MoveUp:
+                    PlayAnimation(moveUpAnim);
+                    break;
+                case State.MoveDown:
+                    PlayAnimation(moveDownAnim);
+                    break;
+            }
+            currentState = value;
+        }
+    }
 
     void Start()
     {
         playerMovement = playerInfo.playerMovement;    
     }
 
+    void PlayAnimation(Animation anim)
+    {
+        currentAnimSequence?.Kill();
+
+        currentAnimSequence = DOTween.Sequence();
+        foreach (var sprite in anim.sprites)
+        {
+            currentAnimSequence.AppendCallback(() => { spriteRenderer.sprite = sprite; }).AppendInterval(anim.timeForSprite);
+        }
+        currentAnimSequence.SetLoops(-1);
+        currentAnimSequence.Play();
+    }
+
+    void StopAnimation()
+    {
+        currentAnimSequence.Kill();
+        currentAnimSequence = null;
+    }
+
     void UpdateBody()
     {
-        spriteRenderer.flipX = false;
         if (playerMovement.RawMoveInput == Vector2.zero)
         {
-            spriteRenderer.sprite = idlePose;
+            CurrentState = State.Idle;
             return;
         }
 
         if (playerMovement.RawMoveInput.y > 0.0f)
         {
-            spriteRenderer.sprite = moveUpPose;
+            CurrentState = State.MoveUp;
             return;
         }
 
         if (playerMovement.RawMoveInput.y < 0.0f)
         {
-            spriteRenderer.sprite = moveDownPose;
+            CurrentState = State.MoveDown;
             return;
         }
 
         if (playerMovement.RawMoveInput.x > 0.0f)
         {
-            spriteRenderer.flipX = true;
-            spriteRenderer.sprite = moveLeftPose;
+            CurrentState = State.MoveRight;
             return;
         }
 
         if (playerMovement.RawMoveInput.x < 0.0f)
         {
-            spriteRenderer.sprite = moveLeftPose;
+            CurrentState = State.MoveLeft;
         }
     }
 
